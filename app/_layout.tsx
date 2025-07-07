e5303eb5ae9b5e9e5d9c4ea3e56f06c6d1d25aa4
wys-prog <wys-workshop@outlook.fr>
2025-07-07 22:27:09 +0200

    fix de CPS et tout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,13 @@
-import { Stack } from "expo-router";
+import { ReactNode } from "react";
 
-export default function RootLayout() {
-  return <Stack />;
+export default function Layout({ children }: { children: ReactNode }) {
+  return (
+    <>
+      <meta
+        httpEquiv="Content-Security-Policy"
+        content="script-src 'self' https://apis.google.com https://www.gstatic.com https://accounts.google.com; object-src 'none';"
+      />
+      {children}
+    </>
+  );
 }
